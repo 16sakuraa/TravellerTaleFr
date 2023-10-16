936f62d59e024f3ad0847ac282af0e8a3714b444
16sakuraa <64010860@kmitl.ac.th>
2023-10-16 17:29:06 +0700

    Fix Review Card

--- components/ReviewCard.tsx
+++ components/ReviewCard.tsx
@@ -34,7 +34,7 @@ export const ReviewCard: React.FC<ReviewCardProps> = ({ username, star, content,
   
     };
 
-    const displayContent = expanded ? content : content.slice(0, 130);
+    const displayContent = expanded ? content : content.slice(0, 110);
 
 
     return (
